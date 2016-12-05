6552f5e255acaaee6d7b0be8d150aa3401251e32
unknown <boullet.nicolas@gmail.com>
2016-12-05 21:22:59 +0100

    Ajout d'un dossier interface, contenant la maquette pour les interfaces, ainsi qu'un code HTML et CSS basique accompagné d'un dossier "img" comprenant l'image de la loupe.

---
+++ Interface/index.php
@@ -0,0 +1,28 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf8">
+		<title>Woble</title>
+		<meta name="viewport" content="width=device-width">
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="icon" type="image/png" href="img/icon.png">
+		<!--Font style-->
+		<link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet"> 
+	</head>
+	<body>
+		<div id="main_box">
+			<h1 id="main_title">WOBLE</h1>
+			<form id="search_form" action="" method="post">
+				<input class="search_bar" name="recherchee" type="text" placeholder="Search...">
+				<input class="search_button" type="submit" value="">
+			</form>
+		</div>
+	</body>
+</html>
