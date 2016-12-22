798925ee3793b5d6fd9f9b16f4b69de9ca0b830a
Nashunn <boullet.nicolas@gmail.com>
2016-12-22 23:22:59 +0100

    Changement important dans l'organisation des dossiers.
    Création d'un dossier Interface afin d'y mettre les documents en rapport avec l'interface (sans implementation Laravel)
    Création d'un dossier Crawler afin d'y mettre les documents en rapports avec les tests ou les fonctions pour le Crawler (sans implémentation Laravel)
    Ajout d'un fichier INFO.txt à la racine afin d'expliquer ce que contient les fichiers, à tenir à jour afin de ne pas être perdu si possible.

--- Interface/index.php
+++ Interface/index.php
@@ -8,19 +8,14 @@
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="icon" type="image/png" href="img/icon.png">
 		<!--Font style-->
-		<link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet"> 
-		<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">  
 	</head>
 	<body>
 		<div id="main_box">
-			<h1 id="main_title">WOBLE</h1>
-			<form id="search_form" action="" method="post">
-				<input class="search_bar" name="recherchee" type="text" placeholder="Search...">
+			<a href="index.php"><h1 id="main_title">WOBLE</h1></a>
+			<form id="search_form" action="search.php" method="post">
+				<input class="search_bar" name="recherche" type="text" placeholder="Search...">
 				<input class="search_button" type="submit" value="">
 			</form>
 		</div>
