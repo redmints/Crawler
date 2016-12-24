66436699caaebad61671f3df5e1cdde0b21b9721
Nashunn <boullet.nicolas@gmail.com>
2016-12-24 13:04:39 +0100

    Changement de l'interface de lancement du crawler.
    Ajout d'éléments dans l'interface de lancement du crawler, réorganisation de la vue. Ajout d'une feuille de style spécifique appelé style_crawler.css.

--- resources/views/VueCrawler/lancement.blade.php
+++ resources/views/VueCrawler/lancement.blade.php
@@ -1,15 +1,22 @@
 @extends('VueCrawler/templateCrawler')
 
 @section('titre')
-  Crawler
+  Crawler - Lancement
 @endsection
 
 @section('contenu')
-    {!! Form::open(['url' => 'crawler']) !!}
-        {!! Form::label('url', 'Entrez une url de départ : ') !!}
-        {!! Form::text('url') !!}
-        {!! Form::label('nbPages', 'Entrez un nombre de pages à crawler : ') !!}
-        {!! Form::text('nbPages') !!}
-        {!! Form::submit('Go !') !!}
-    {!! Form::close() !!}
+	<a href="./"><h1>WOBLE</h1></a>
+	<div class="formulaire">
+		<h2>Lancement du crawler</h2>
+		<p>
+			{!! Form::open(['url' => 'crawler']) !!}
+				{{ csrf_field() }}
+				{!! Form::label('url', 'Entrez une url de départ : ') !!} <br>
+				{!! Form::text('url') !!} <br>
+				{!! Form::label('nbPages', 'Entrez un nombre de pages à crawler : ') !!} <br>
+				{!! Form::text('nbPages') !!}<br><br>
+				{!! Form::submit('Lancer', ['class'=>'red_button']) !!}
+			{!! Form::close() !!}
+		</p>
+	</div>
 @endsection
--- resources/views/VueCrawler/templateCrawler.blade.php
+++ resources/views/VueCrawler/templateCrawler.blade.php
@@ -1,8 +1,12 @@
 <!doctype html>
 <html lang="fr">
 <head>
+	<link href="style_crawler.css" rel="stylesheet" type="text/css">
     <meta charset="UTF-8">
     <title>@yield('titre')</title>
+	<!--Font style-->
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
 </head>
 <body>
     @yield('contenu')
