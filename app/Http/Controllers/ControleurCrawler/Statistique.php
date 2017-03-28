713f44bef1fb486e6ae1eae235e2cc56bf689b9d
Nashunn <boullet.nicolas@gmail.com>
2017-03-28 22:18:33 +0200

    Retour à une version basique des statistiques

--- app/Http/Controllers/ControleurCrawler/Statistique.php
+++ app/Http/Controllers/ControleurCrawler/Statistique.php
@@ -13,7 +13,8 @@ class Statistique extends Controller
 	$sites = DB::table('website')->where('etat','=','1')->get();
 	$nbsites = count($sites);
 	$lastsites = DB::table('website')->where('etat','=','1')->orderBy('id', 'desc')->take(10)->get();
-	foreach($lastsites as $sites) {
+	
+	/*foreach($lastsites as $sites) {
 		//On recupere l'id du mot le plus important dans le site
 		$keyword_id = DB::table('link')->where('websiteid','=',$sites->id)->orderBy('importance', 'desc')->first();
 		$keyword = DB::table('keywords')->where('id','=',$keyword_id->keywordid)->first(); //le mot en question
@@ -22,8 +23,8 @@ class Statistique extends Controller
 		$retour["text"] = $keyword->text;
 		$retour["importance"] = $keyword_id->importance;
 		$links[] = $retour; //On le met en tab
-	}
+	}*/
 	
-	return view('VueCrawler/statistique', compact('sites', 'nbsites', 'lastsites', 'links'));
+	return view('VueCrawler/statistique', compact('sites', 'nbsites', 'lastsites'));
   }
 }
