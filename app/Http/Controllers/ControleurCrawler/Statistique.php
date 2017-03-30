5c85b44c2fe7b768200e4c98410b4a12c7e17da0
Nashunn <boullet.nicolas@gmail.com>
2017-03-30 13:34:15 +0200

    Retour à une version, si reprise, changer la ligne 19 de statstistiques. (sites = lastsites)

--- app/Http/Controllers/ControleurCrawler/Statistique.php
+++ app/Http/Controllers/ControleurCrawler/Statistique.php
@@ -14,17 +14,17 @@ class Statistique extends Controller
 	$nbsites = count($sites);
 	$lastsites = DB::table('website')->where('etat','=','1')->orderBy('id', 'desc')->take(10)->get();
 	
-	/*foreach($lastsites as $sites) {
+	foreach($lastsites as $sites) {
 		//On recupere l'id du mot le plus important dans le site
-		$keyword_id = DB::table('link')->where('websiteid','=',$sites->id)->orderBy('importance', 'desc')->first();
+		$keyword_id = DB::table('link')->where('websiteid','=',$lastsites->id)->orderBy('importance', 'desc')->first();
 		$keyword = DB::table('keywords')->where('id','=',$keyword_id->keywordid)->first(); //le mot en question
 		$website = DB::table('website')->where('id','=',$keyword_id->websiteid)->first(); //le site en question
 		$retour["title"] = $website->title;
 		$retour["text"] = $keyword->text;
 		$retour["importance"] = $keyword_id->importance;
 		$links[] = $retour; //On le met en tab
-	}*/
+	}
 	
-	return view('VueCrawler/statistique', compact('sites', 'nbsites', 'lastsites'));
+	return view('VueCrawler/statistique', compact('sites', 'nbsites', 'lastsites', 'links'));
   }
 }
