88a2a7ab7c933dca1f1f338d096794864a34e1ab
Nashunn <boullet.nicolas@gmail.com>
2017-03-30 13:12:51 +0200

    Reprise d'un test initial sur statistiques

--- resources/views/VueCrawler/statistique.blade.php
+++ resources/views/VueCrawler/statistique.blade.php
@@ -11,22 +11,27 @@
 		<!-- Scripts -->
 		<script src="http://canvasjs.com/assets/script/canvasjs.min.js"></script>
         <script type="text/javascript">
-            window.onload = function () {
-                var columnchart = new CanvasJS.Chart("columnchart_mots", {
-                    data: [
-						{
-							type: "column",
-							toolTipContent: "{label} - {y}",
-							dataPoints: [
-								@foreach ($links as $link)
-									{ label : "{{ $link['title'] }} - {{ $link['text'] }}",  y: {{ $link['importance'] }} },
-								@endforeach
-							]
-						}
-                    ]
-                });
-                columnchart.render();
-            }
+			window.onload = function () {
+				var chart = new CanvasJS.Chart("columnchart_mots", {
+					theme: "theme2",//theme1
+					title:{ text: "Basic Column Chart - CanvasJS" },
+					animationEnabled: false,   // change to true
+					data: [              
+					{
+						// Change type to "bar", "area", "spline", "pie",etc.
+						type: "column",
+						dataPoints: [
+							{ label: "apple",  y: 10  },
+							{ label: "orange", y: 15  },
+							{ label: "banana", y: 25  },
+							{ label: "mango",  y: 30  },
+							{ label: "grape",  y: 28  }
+						]
+					}
+					]
+				});
+				chart.render();
+			}
         </script>
 	</head>
 	<body>
